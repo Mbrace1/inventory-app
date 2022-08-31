699e8b0140db7f6e8e77c0ed18887f51206d4c71
Afrosweetness2002 <abdullatiffarah21@gmail.com>
2022-08-31 14:18:05 +0100

    I am done with getting the route to work containing all items.

---
+++ src/components/ItemDetailed.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+function ItemDetailed(props) {
+    console.log(props)
+    
+  return (
+    <Card elevation="6" sx={{ maxWidth: "80vw", minWidth: 300, mx: "auto", my: "1em"}}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="*link goes here"
+        alt="title"// {props.title}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          dummy title {/* {props.title} */}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+            dummy description {/*{props.description*/}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ItemDetailed
