31568d53588109f2517ce23f5a3fd8e7251ecb3d
Mbrace1 <michaelbracey24@gmail.com>
2022-09-02 09:20:46 +0100

    Added filter/search bar

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Item from './Item'
 import Grid from '@mui/material/Grid';
 
@@ -6,23 +6,34 @@ import './ItemList.css'
 
 function ItemList(props) {
     console.log(props)
+    const [filter, setFilter] = useState("")
 
     return (
-        <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        wrap="wrap"
-        >
-            {props.data.map((data, key) => {
-                // need to pass props rather than dummy
-                return <Item fetchSingleItem={props.fetchSingleItem}
-                key={key} id={data.id} title={data.title} 
-                description={data.description} 
-                price={data.price} image={data.image}/>
-            })}
-        </Grid>
+        <>
+            <label for="searchFor">Search Items: </label>
+            <input id="searchFor" type="text" onChange={(e) => setFilter(e.target.value)}></input>
+            <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            wrap="wrap"
+            >
+            {props.data.filter(data => {
+                if (filter == "") {
+                    return data
+                } else if (data.title.toLowerCase().includes(filter.toLowerCase())) {
+                    return data
+                }
+                }).map((data, key) => {
+                    // need to pass props rather than dummy
+                    return <Item fetchSingleItem={props.fetchSingleItem}
+                    key={key} id={data.id} title={data.title} 
+                    description={data.description} 
+                    price={data.price} image={data.image}/>
+                })}
+            </Grid>
+        </>
     )
 }
 
